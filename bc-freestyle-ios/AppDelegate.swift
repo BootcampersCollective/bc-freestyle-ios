475089425776269aab0bfe52509574995f54bc1b
Christopher Bonuel <bonuelc@gmail.com>
2017-09-30 13:12:36 -0600

    Initial Commit

---
+++ bc-freestyle-ios/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  bc-freestyle-ios
+//
+//  Created by Christopher Bonuel on 9/30/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ bc-freestyle-ios/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  bc-freestyle-ios
+//
+//  Created by Christopher Bonuel on 9/30/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
