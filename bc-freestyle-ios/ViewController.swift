c50461911edab25c2f50897a6688da5e39dd43d8
Christopher Bonuel <bonuelc@gmail.com>
2017-09-30 14:33:02 -0600

    Present AlertController with dummy actions on click

--- bc-freestyle-ios/ViewController.swift
+++ bc-freestyle-ios/ViewController.swift
@@ -12,5 +12,19 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    @IBAction func addBarButtonItemTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "", message: "Have or want?", preferredStyle: .actionSheet)
+        
+        let wantSegue = UIAlertAction(title: "Have", style: .default, handler: nil)
+        let haveSegue = UIAlertAction(title: "Want", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(wantSegue)
+        alertController.addAction(haveSegue)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
